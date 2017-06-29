fa5b9a1228d23125c102c892af30e5cf5bb5a775
Arthur <arthurlac@icloud.com>
2017-06-29 20:18:31 +0100

    Types and psuedo code for boolean lang

--- logic.ml
+++ logic.ml
@@ -3,10 +3,48 @@
  * Define predicates
  *   not, and, or, nand, nor, xor, eq
  * which succeed or fail according to the result of their respective
- * operations; e.g. and(A,B) will succeed, iff both A and B succeed.
+ * operations; e.g. and(A,B) will succeed, iff both A and B are true.
  *)
-let truth_table exp =
+exception Variable_Unbound
+type expr =
+    | And   of expr * expr
+    | Or    of expr * expr
+    | Not   of expr
+    | Var   of string
+    | Const of bool
+(*  | Nand  of expr * expr
+ *  | Nor   of expr * expr
+ *  | Xor   of expr * expr
+ *  | Eq    of expr * expr *)
 
+(* TODO Benchmark set vs list impl *)
+(* TODO Return list or set? *)
+let find_vars expr =
+    let open String.Set in
+    let rec aux expr vars = match expr with
+        | Var   x -> add vars x
+        | Const x -> vars
+        | And _ | Or _ as (x, y) -> union (aux x vars) (aux y vars)
+        | Not x -> aux vars x
+    in aux expr empty
+
+let gen_ctxts vars = 
+
+let eval expr ctxt =
+    let unbind x = match List.Assoc.find ctxt ~equal:String.equal x with
+        | Some b -> b
+        | None -> raise Variable_Unbound in
+    let rec eval' expr = match expr with
+        | And   (x, y) -> eval x && eval y
+        | Or    (x, y) -> eval x || eval y
+        | Not   x      -> not (eval x)
+        | Var   x      -> unbind x
+        | Const x      -> x
+    in eval' expr
+
+let gen_truth_table expr = find_vars expr
+    |> gen_ctxts
+    |> List.map ~f:(ctxt -> eval expr ctxt) (* TODO Memoify? *)
 
 (* Gray code.
  *
