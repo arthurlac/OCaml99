8138914ae03e02715174c7f9d3592c9bbe3dedc8
Arthur <arthurlac@icloud.com>
2017-07-01 23:03:15 +0100

    Slice impl and refactoring

--- lists.ml
+++ lists.ml
@@ -85,21 +85,16 @@ let drop_every_nth l ~n =
     in drop_aux (reverse l) 0 Empty
 
 (* Split a list into two parts; the length of the first part is given. *)
-(* TODO Clean *)
 let split l ~n =
-    let rec split_aux left right dec = if dec <= 0
-        then ((reverse left), right) else match right with
-            | Empty -> ((reverse left), right)
-            | Cons (hd, tl) -> split_aux (Cons (hd, left)) tl (dec - 1)
-    in split_aux Empty l n
+    let rec aux left right ix = if ix = n then (left, right) else
+        match right with
+            | Empty -> (left, right)
+            | Cons (hd, tl) -> aux (Cons (hd, left)) tl (ix + 1)
+    in aux Empty l 0
 
 (* Extract a slice from a list. *)
-(* TODO *)
 let slice l ~lb ~ub =
-    let slice_aux l lb ub ix = if ix <
-        if ix = ub then Some l else match l with
-            | Empty -> None
-            | Cons (hd, tl) ->
+    split l ~n:lb |> snd |> split ~n:ub |> fst
 
 (* Rotate a list N places to the left. *)
 (* TODO check for off by one err *)
@@ -109,26 +104,24 @@ let rotate l ~n =
     Llist.append right left
 
 (* Remove the K'th element from a list. *)
-(* TODO *)
-let drop_at_nth l ~n =
-    let aux l ix r = if ix = n then (car r, append l r) else
-        match 
+exception Out_Of_Bounds
+let drop_at_nth_exn l ~n =
+    let rec aux l ix r = if ix = n then append l (cdr r) else
+        match r with
+            | Empty -> raise Out_Of_Bounds
+            | Cons (hd, tl) -> aux (Cons (hd, l)) (ix + 1) tl
+    in if n < 0 then raise Out_Of_Bounds else aux Empty 0 l
 
 (* Insert an element at a given position into a list. *)
-(* TODO append at end if n > len l ? insert_at_nth_exn *)
-(* TODO see if an be built in order to not need rev *)
-(* TODO check n => 0 *)
-let insert_at_nth l ~n ~elem =
+let insert_at_nth_exn l ~n ~elem =
     let rec aux l ix r = if ix = n then append l (Cons (elem, r)) else
-        match l with
-            | Empty -> Cons (elem, r)
-            | Cons (hd, tl) -> aux tl (ix + 1) (Cons (hd, r))
-    in rev (aux l 0 Empty)
-  (*in      aux Empty 0 l XXX see todo no2 *)
-
+        match r with
+            | Empty -> raise Out_Of_Bounds
+            | Cons (hd, tl) -> aux (Cons (hd, l)) (ix + 1) tl
+    in if n < 0 then raise Out_Of_Bounds else aux Empty 0 l
 
 (* Create a list containing all integers within a given range. *)
-let seq ~lb ~ub =
+let seq lb ub =
     let rec seq_aux lb ub l = if lb = ub then l else
         seq_aux lb (ub - 1) (Cons (ub, l))
     in if lb =< ub then seq_aux lb ub Empty else seq_aux ub lb Empty
@@ -139,12 +132,12 @@ let rndm_extract l ~count =
     let rec aux count l selected = if count =< 0 then (selected, l) else
         let rnd_ix = Random.int (len l) in
         let (pick, nl) = drop_at_nth l ~n:rnd_ix in
-        lotto_aux (count - 1) nl (Cons (pick, selected)) in
+        aux (count - 1) nl (Cons (pick, selected)) in
     aux count l Empty
 
 (* Lotto: Draw N different random numbers from the set 1..M. *)
 let lotto ~count ~ub = 
-    let (picks, _) = rndm_extract (seq ~lb:1 ~ub) ~count in
+    let (picks, _) = rndm_extract (seq 1 ub) ~count in
     picks
 
 (* Generate a random permutation of the elements of a list. *)
