c2b854f3acde945b64d6a468c2f5f774ad3b52ca
Arthur <arthurlac@icloud.com>
2017-07-04 15:00:20 +0100

    is_sym and base funs

--- src/b_trees.ml
+++ src/b_trees.ml
@@ -1,15 +1,42 @@
 (* Binary trees *)
+open Option.Monad_infix
+
 type 'a t =
     | Empty
     | Node of 'a * 'a t * 'a t
 
+(* Sample data for debugging *)
+let t1  = Node (1, Empty, Empty);;
+let t2  = Node (2, t1, t1);;
+let t3  = Node (3, t2, t2);;
+let t23 = Node (4, t2, t3);;
+
+let car   = function Empty -> None | Node (v, _, _) -> Some v
+let left  = function Empty -> None | Node (_, l, _) -> Some l
+let right = function Empty -> None | Node (_, _, r) -> Some r
+
+let lcar t = left t  >>= car
+let rcar t = right t >>= car
 
 (* Construct completely balanced binary trees. *)
+let cons n v = 
 
 (* Symmetric binary trees. *)
+exception Break
+
+let is_sym t =
+    let rec aux t = match t with
+        | Empty -> true
+        | Node (v, l, r) -> if (car l) = (car r) then
+            (aux l) && (aux r)
+        else raise Break (* Short circuits as we found ineq *)
+    in try (aux t) with Break -> false
+
 (* Binary search trees (dictionaries).
  * Construct a binary search tree from a list of integer numbers.
  *)
+let cons l =
+
 (* Generate-and-test paradigm. *)
 (* Construct height-balanced binary trees. *)
 (* Construct height-balanced binary trees with a given number of nodes. *)
@@ -20,6 +47,7 @@ type 'a t =
 (* Construct a complete binary tree. *)
 (* Layout a binary tree. *)
 (* A string representation of binary trees. *)
-(* Preorder and inorder sequences of binary trees. *)
+(* Preorder and inorder Osequences of binary trees. *)
+let t4  = Node (4, t3, t3);;
 (* Dotstring representation of binary trees. *)
 
