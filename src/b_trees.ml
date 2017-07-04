dc254d9342edf4e1ac20ef38a48b954e469253cf
Arthur <arthurlac@icloud.com>
2017-07-04 14:11:00 +0100

    mv src/{.*}/*.ml -> src/*.ml and add b trees problems

---
+++ src/b_trees.ml
@@ -0,0 +1,25 @@
+(* Binary trees *)
+type 'a t =
+    | Empty
+    | Node of 'a * 'a t * 'a t
+
+
+(* Construct completely balanced binary trees. *)
+
+(* Symmetric binary trees. *)
+(* Binary search trees (dictionaries).
+ * Construct a binary search tree from a list of integer numbers.
+ *)
+(* Generate-and-test paradigm. *)
+(* Construct height-balanced binary trees. *)
+(* Construct height-balanced binary trees with a given number of nodes. *)
+(* Count the leaves of a binary tree. *)
+(* Collect the leaves of a binary tree in a list. *)
+(* Collect the internal nodes of a binary tree in a list. *)
+(* Collect the nodes at a given level in a list. *)
+(* Construct a complete binary tree. *)
+(* Layout a binary tree. *)
+(* A string representation of binary trees. *)
+(* Preorder and inorder sequences of binary trees. *)
+(* Dotstring representation of binary trees. *)
+
