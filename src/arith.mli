fb4409455a6e103331bb4b5ce30ea128fade07d3
Arthur <arthurlac@icloud.com>
2017-07-05 17:46:08 +0100

    Add arith and llist mli

---
+++ src/arith.mli
@@ -0,0 +1,7 @@
+val primes_seq : int -> int list = <fun>
+val gcd : int -> int -> int = <fun>
+val coprime : int -> int -> bool = <fun>
+val phi : int -> int = <fun>
+val prime_factors : int -> int list = <fun>
+val prime_factor_pairs : int -> (int, int) List.Assoc.t = <fun>
+val phi' : int -> int = <fun>
