740e270f86c9185788079f76cd407c9a5a94be0c
Arthur <arthurlac@icloud.com>
2017-07-15 14:20:05 +0100

    Arith mli

--- src/arith.mli
+++ src/arith.mli
@@ -1,7 +1,22 @@
+open Core
+
+val is_prime : int -> bool
+
 val primes_seq : int -> int list
+
 val gcd : int -> int -> int
+
 val coprime : int -> int -> bool
+
+val seq : int -> int -> int list
+
 val phi : int -> int
+
 val prime_factors : int -> int list
-val prime_factor_pairs : int -> (int, int) Core.List.Assoc.t
+val prime_factor_pairs : int -> (int, int) List.Assoc.t
+
 val phi' : int -> int
+
+val goldbach          : int -> int * int
+val goldbach_w_primes : int -> int list -> int * int
+val goldbach_list     : int -> int -> (int * int) list
