b431fae34c5e72961c91048bb49e1b190abcfb6d
Arthur <arthurlac@icloud.com>
2017-07-18 22:49:59 +0100

    is_connected and path via bfs

--- src/graph.ml
+++ src/graph.ml
@@ -8,19 +8,8 @@ module type Node = sig
     val compare : t -> t -> int
 end
 
-module type Graph_intf = sig
-   type t
-   type node
-
-   val nodes : t -> node list
-
-   val to_edge_list   : t -> (node * node) list
-
-   val from_edge_list : (node * node) list -> t
-end
-
 (* List all edges, an edge being a pair of nodes *)
-module Edge_list (N : Node) : Graph_intf = struct
+module Edge_list (N : Node) = struct
   type node = N.t
   type t = (node * node) list
 
@@ -34,7 +23,7 @@ module Edge_list (N : Node) : Graph_intf = struct
 end
 
 (* Graph term form: nodes and edges *)
-module Graph_term (N : Node) : Graph_intf = struct
+module Graph_term (N : Node) = struct
     module El = Edge_list(N)
     type node = El.node
     type t = { nodes : node list; edges : El.t }
@@ -50,23 +39,31 @@ module Graph_term (N : Node) : Graph_intf = struct
 end
 
 (* List of nodes and the nodes which they share an edge with *)
-module Adjc_list (N : Node) : sig
-  include Graph_intf
-
-end = struct
+module Adjc_list (N : Node) (*: sig
+  type node = N.t
+  type t
+  val nodes : t -> node list
+  val from_edge_list : (node * node) list -> t
+  val to_edge_list : t -> (node * node) list
+  val path : t -> node -> node -> bool
+end *)= struct
     type node = N.t
     type t = (node, node list) Hashtbl.t
 
     let nodes = Hashtbl.keys
 
+    let nodes_mem l n = List.mem ~equal:(N.equal) l n
+
     let from_edge_list el =
       let tbl = Hashtbl.create ~hashable:(N.hashable) () in
-      List.iter el ~f:(fun (x, y) ->
-          let neighbours = match Hashtbl.find tbl x with
-          | None -> []
-          | Some l -> l
-          in Hashtbl.set tbl ~key:x ~data:(y :: neighbours)
-        );
+      (* TODO Comm *)
+      let add_edge f t = (* f : from , t : to *)
+          let neighbours = match Hashtbl.find tbl f with
+            | None -> [] | Some l -> l
+          in if nodes_mem neighbours t then () else
+          Hashtbl.set tbl ~key:f ~data:(t :: neighbours)
+      in
+      List.iter el ~f:(fun (x, y) -> add_edge x y; add_edge y x);
       tbl
 
     let pair_eq (a, b) (c, d) =
@@ -84,67 +81,45 @@ end = struct
             )
         )
 
+    (* TODO Clean *) (* TODO Review *)
+    let is_connected g origin target =
+      let rec bfs visited curn =
+        if nodes_mem visited curn then false else
+        match Hashtbl.find g curn with
+          | None -> false
+          | Some nodes ->
+            if nodes_mem nodes target then true else
+              let v' = curn :: visited in
+              List.fold nodes ~init:false
+                ~f:(fun acc n -> (bfs v' n) || acc)
+      in bfs [] origin
+
+    exception Ret of node list option
+
+    let path g origin target =
+      let rec bfs visited path curn =
+        if nodes_mem visited curn then None else
+        match Hashtbl.find g curn with
+          | None -> None
+          | Some nodes ->
+            let p' = curn :: path in
+            if nodes_mem nodes target then
+              raise (Ret (Some (List.rev (target :: p'))))
+            else
+              let v' = curn :: visited in
+              List.fold nodes ~init:None
+                ~f:(fun _ n -> bfs v' p' n)
+      in try bfs [] [] origin with Ret a -> a
+
 end
 
-(* Conversions.
- *
- * Write functions to convert between the different graph representations.
- * With these functions, all representations are equivalent; i.e. for the
- * following problems you can always pick freely the most convenient form.
- * This problem is not particularly difficult, but it's a lot of work to
- * deal with all the special cases.
- *
- * Solution : use (to|from)_edge_list for each type
- *)
-
-(* Path from one node to another one. Write a function paths g a b that returns
- * all acyclic path p from node a to node b ≠ a in the graph g.
- *
-let paths g a b =
-   *)
-(*
-
-
-(* Cycle from a given node.
- * Write a functions cycle g a that returns a closed path (cycle) p starting at a
- * given node a in the graph g.
- *)
-
-(* Construct all spanning trees.
- * Write a function s_tree g to construct all spanning trees of a given graph g.
- * With this predicate, find out how many spanning trees there are for the graph
- * depicted to the left.
- *)
-
-(* Construct the minimal spanning tree.  *)
-
-(* Graph isomorphism.
- * Two graphs G1(N1,E1) and G2(N2,E2) are isomorphic if there is a bijection f:
- * N1 → N2 such that for any nodes X,Y of N1, X and Y are adjacent if and only if
- * f(X) and f(Y) are adjacent.
- *)
-
-(* Write a function that determines whether two graphs are isomorphic. *)
-
-(* Node degree and graph coloration.
- * Write a function degree graph node that determines the degree of a given node.
- * Write a function that generates a list of all nodes of a graph sorted according
- * to decreasing degree.
- *)
-
-(* Depth-first order graph traversal.
- * Write a function that generates a depth-first order graph traversal sequence.
- * The starting point should be specified, and the output should be a list of
- * nodes that are reachable from this starting point (in depth-first order).
- *)
-
-(* Write a predicate that splits a graph into its connected components. *)
-
-(* Write a predicate that finds out whether a given graph is bipartite.*)
-
-(* Generate K-regular simple graphs with N nodes.
- * In a K-regular graph all nodes have a degree of K; i.e. the number of edges
- * incident in each node is K. How many (non-isomorphic!) 3-regular graphs with 6
- * nodes are there?
- *)
- *)
+(* Usage
+  module G = Adjc_list(Int);;
+  let g' = G.from_edge_list [1,2; 2,3; 2,4; 5,6];;
+  G.nodes g';;
+  - : int list = [6; 5; 4; 3; 2; 1]
+  G.path g' 3 2;;
+  - : bool = true
+  G.path g' 3 5;;
+  - : bool = false
+*)
